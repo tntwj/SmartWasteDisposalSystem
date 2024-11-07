8bf2ccaf9a7aae5cf7187bddedcde95c093e4d25
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-07 13:27:57 +0100

    scheduler implemented, task modified

--- src/tasks/Task.h
+++ src/tasks/Task.h
@@ -2,9 +2,26 @@
 #define __TASK__
 
 class Task {
+    int myPeriod;
+    int timeElapsed;
+
 public:
-    virtual void init() = 0;
+    virtual void init(int period) {
+        myPeriod = period;
+        timeElapsed = 0;
+    }
+
     virtual void tick() = 0;
+
+    bool updateAndCheckTime(int basePeriod) {
+        timeElapsed += basePeriod;
+        if (timeElapsed >= myPeriod) {
+            timeElapsed = 0;
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 #endif
