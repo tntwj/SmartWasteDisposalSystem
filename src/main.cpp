e2480aa8b968495212125876cec7e2b3069e8905
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-08 12:51:39 +0100

    creating tasks in setup and add to scheduler

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <Arduino.h>
+#include "defines.h"
 #include "Scheduler.h"
+#include "MeasureLevelTask.h"
+#include "MeasureTemperatureTask.h"
+#include "WasteDisposalTask.h"
 
 Scheduler sched;
 
@@ -7,6 +11,18 @@ void setup() {
   Serial.begin(9600);
   Serial.println("ArduinoUno");
   sched.init(50);
+
+  /*creating tasks and add to scheduler*/
+  Task* measureLevel = new MeasureLevelTask();
+  measureLevel->init(MEASURE_LEVEL_PERIOD);
+  sched.addTask(measureLevel);
+  Task* measureTemp = new MeasureTemperatureTask();
+  measureTemp->init(MEASURE_TEMPERATURE_PERIOD);
+  sched.addTask(measureTemp);
+  Task* wasteDisposalTask = new WasteDisposalTask();
+  wasteDisposalTask->init(WASTE_DISPOSAL_TASK_PERIOD);
+  sched.addTask(wasteDisposalTask);
+
 }
 
 void loop() {
