67f37eebda09f55cae2375ba667e9e92de2c89d7
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-11-08 21:04:40 +0100

    Moved files again

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
 Scheduler sched;
 
 void setup() {
+  /**
+   * Reminder that Serial.Begin() is later called in MsgService
+   */
   Serial.begin(9600);
   Serial.println("ArduinoUno");
   sched.init(50);
 
   /*creating tasks and add to scheduler*/
+  /**
+   * YOU NEED TO DECLARE THE CONSTRUCTOR IN THE HEADER!!!
+   */
   Task* measureLevel = new MeasureLevelTask();
   measureLevel->init(MEASURE_LEVEL_PERIOD);
   sched.addTask(measureLevel);
