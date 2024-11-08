91ccaedb73df52b1f04208db77ef9c48069a440b
Jiekai Sun <jiekai.sun@studio.unibo.it>
2024-11-08 20:39:51 +0100

    Imported MsgService

---
+++ lib/communication/MsgService.cpp
@@ -0,0 +1,48 @@
+#include "MsgService.h"
+
+static const String INIT_MESSAGE = "ArduinoUno";
+
+String content;
+MsgServiceClass MsgService;
+
+bool MsgServiceClass::isMsgAvailable() {
+    return this->msgAvailable;
+}
+
+Msg* MsgServiceClass::receiveMsg() {
+    if (this->msgAvailable) {
+        Msg* msg = this->currentMsg;
+        this->msgAvailable = false;
+        this->currentMsg = nullptr;
+        content = "";
+        return msg;
+    } else {
+        return nullptr;
+    }
+}
+
+void MsgServiceClass::init() {
+    Serial.begin(9600);
+    Serial.println(INIT_MESSAGE);
+    content.reserve(256);
+    content = "";
+    this->currentMsg = nullptr;
+    this->msgAvailable = false;
+}
+
+void MsgServiceClass::sendMsg(const String& msg) {
+    Serial.println(msg);
+}
+
+void serialEvent() {
+    /* reading the content */
+    while (Serial.available()) {
+        char ch = (char) Serial.read();
+        if (ch == '\n') {
+            MsgService.currentMsg = new Msg(content);
+            MsgService.msgAvailable = true;
+        } else {
+            content += ch;
+        }
+    }
+}
---
+++ lib/communication/MsgService.h
@@ -0,0 +1,41 @@
+#ifndef __MSG_SERVICE__
+#define __MSG_SERVICE__
+
+#include "Arduino.h"
+
+extern MsgServiceClass MsgService;
+
+class Msg {
+    public:
+        Msg(String content) {
+            this->content = content;
+        }
+
+        String getContent() {
+            return this->content;
+        }
+
+    private:
+        String content;
+};
+
+class Pattern {
+    public:
+        virtual boolean match(const Msg& m) = 0;  
+};
+
+class MsgServiceClass {
+    public:
+        void init();
+        bool isMsgAvailable();
+        Msg* receiveMsg();
+        bool isMsgAvailable(Pattern& pattern);
+        Msg* receiveMsg(Pattern& pattern);
+        void sendMsg(const Msg& msg);
+        void sendMsg(const String& msg);
+    private:
+        Msg* currentMsg;
+        bool msgAvailable;
+};
+
+#endif
