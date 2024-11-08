58a8421e7f5ddc66706871624641f5661766fa83
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-08 13:24:15 +0100

    let waste disposal task to istantiate controllers

--- lib/tasks/WasteDisposalTask.h
+++ lib/tasks/WasteDisposalTask.h
@@ -3,15 +3,25 @@
 
 #include "Task.h"
 #include "State.h"
+#include "ButtonPadController.h"
+#include "MotionDetector.h"
+#include "DoorController.h"
+#include "LedController.h"
+#include "Defines.h"
 
 class WasteDisposalTask: public Task{
     private:
         State* currentState;
 
     public:
+        ButtonPadController* buttonPadController = new ButtonPadController();
+        MotionDetector* motionDetector = new MotionDetector();
+        DoorController* doorController = new DoorController();
+        LedController* ledController = new LedController(new Led(GREEN_LED_PIN), new Led(RED_LED_PIN));
         /*every time this method has been called, the current state should update,
         it could remains in the same state (it depends on what state returned by state.handle()). */
         void tick();
+
 };
 
 #endif
--- lib/wasteDisposalStates/State.h
+++ lib/wasteDisposalStates/State.h
@@ -1,10 +1,6 @@
 #ifndef __STATE__
 #define __STATE__
-#include "ButtonPadController.h"
-#include "MotionDetector.h"
-#include "DoorController.h"
-#include "LedController.h"
-#include "Defines.h"
+
 
 class State{
     public:
@@ -13,13 +9,7 @@ class State{
          * every state should do his own job.
          * @return a new state which could be another state or nullptr if its the same state.
          */
-        virtual State* handle();
-
-    protected:
-        ButtonPadController* buttonPadController = new ButtonPadController();
-        MotionDetector* motionDetector = new MotionDetector();
-        DoorController* doorController = new DoorController();
-        LedController* ledController = new LedController(new Led(RED_LED_PIN), new Led(GREEN_LED_PIN));
+        virtual State* handle();      
 };
 
 #endif
