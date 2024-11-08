3b67d44dae83945b7dc16493c8a193baae5aee61
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-08 13:46:57 +0100

    entering waste implemented, not complete

--- lib/wasteDisposalStates/EnteringWaste.cpp
+++ lib/wasteDisposalStates/EnteringWaste.cpp
@@ -1,1 +1,11 @@
 #include "State.h"
+
+class EnteringWaste: public State{
+    EnteringWaste() {
+        doorController->openFront();
+    }
+
+    State* handle() {
+        return nullptr;
+    }
+};
