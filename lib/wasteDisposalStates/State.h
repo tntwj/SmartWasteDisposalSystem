de0b8dc3e70fcab1e1a8a26f1f8039d9240837e0
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-08 13:16:29 +0100

    initiate controllers in State.h

--- lib/wasteDisposalStates/State.h
+++ lib/wasteDisposalStates/State.h
@@ -1,14 +1,25 @@
 #ifndef __STATE__
 #define __STATE__
+#include "ButtonPadController.h"
+#include "MotionDetector.h"
+#include "DoorController.h"
+#include "LedController.h"
+#include "Defines.h"
 
 class State{
     public:
-    
-    State();
-    /**
-     * every state should do his own job.
-     * @return a new state which could be another state or nullptr if its the same state.
-     */
-    virtual State* handle();
+        State();
+        /**
+         * every state should do his own job.
+         * @return a new state which could be another state or nullptr if its the same state.
+         */
+        virtual State* handle();
+
+    protected:
+        ButtonPadController* buttonPadController = new ButtonPadController();
+        MotionDetector* motionDetector = new MotionDetector();
+        DoorController* doorController = new DoorController();
+        LedController* ledController = new LedController(new Led(RED_LED_PIN), new Led(GREEN_LED_PIN));
 };
+
 #endif
