4292d5618da6da31809f7734671b8eab266104e0
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-10 18:13:39 +0100

    sleep state can switch states to idle or container full depending on leds

--- lib/wasteDisposalStates/SleepState.cpp
+++ lib/wasteDisposalStates/SleepState.cpp
@@ -1,13 +1,17 @@
 #include "SleepState.h"
 #include "Idle.h"
+#include "ContainerFull.h"
 
 class SleepState: public State {
     SleepState() {
         //sleep
     }
     State* handle() override {
-        if (motionDetector->hasDetected()) {
+        if (motionDetector->hasDetected() && ledController->isGreenOn()) {
             return new Idle();
+        }
+        if (motionDetector->hasDetected() && ledController->isRedOn()) {
+            return new ContainerFull();
         }
         return nullptr;
     }
