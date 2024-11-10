14069c07c2855875758d0feb84d5d8b3f711b69a
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-10 12:55:46 +0100

    temperature controller istantiated, temporary let analog pin of temp as 0!

--- lib/tasks/MeasureTemperatureTask.h
+++ lib/tasks/MeasureTemperatureTask.h
@@ -2,10 +2,11 @@
 #define __MEASURETEMPERATURETASK__
 #include "Task.h"
 #include "TemperatureController.h"
-
+#include "Pins.h"
+#include "Defines.h"
+TemperatureController* tempController = new TemperatureController(new TemperatureSensor(TEMP_SENSOR_PIN), TEMPERATURE_THRESHOLD);
 class MeasureTemperatureTask: public Task {
-    private:
-        TemperatureController tempController;
+
     public:
         void tick();
 };
