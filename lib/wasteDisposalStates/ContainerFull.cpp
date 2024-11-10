8e7663ad16e389e2d60832a6f7590d6d42d33aa4
Weijie Fu <weijie.fu@studio.unibo.it>
2024-11-10 12:50:30 +0100

    entry of wasteReceived and ContainerFull implemented

--- lib/wasteDisposalStates/ContainerFull.cpp
+++ lib/wasteDisposalStates/ContainerFull.cpp
@@ -3,7 +3,9 @@
 class ContainerFull: public State {
     public:
         ContainerFull() {
-
+            ledController->switchOffGreen();
+            ledController->switchOnRed();
+            doorController->close();
         }
     State* handle() override{
         return nullptr;
--- lib/wasteDisposalStates/WasteReceived.cpp
+++ lib/wasteDisposalStates/WasteReceived.cpp
@@ -3,7 +3,7 @@
 class WasteReceived: public State {
     public:
         WasteReceived() {
-
+            doorController->close();
         }
     State* handle() override {
 
